05f365a23773e7d431817be178003b717aa54e6a
root <root@ip-172-31-8-238.us-west-1.compute.internal>
2019-06-29 20:54:31 +0000

    final edits

--- CaveBot/cogs/UniversalBasicIncome.py
+++ CaveBot/cogs/UniversalBasicIncome.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-con = sqlite3.connect('../CaveBot.db')
+con = sqlite3.connect('/home/mason/python/home-projects/CaveBot/CaveBot.db')
 db = con.cursor()
 
-db.execute('UPDATE High_Rollers SET Balance = Balance + 420')
+db.execute('UPDATE High_Rollers SET Balance = Balance + 420') ##blaze it
 con.commit()
